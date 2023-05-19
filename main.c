74a76f75532e360a12c849ee89c054145b9bd488
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-05-19 16:43:24 -0700

    train on partial dataset

--- main.c
+++ main.c
@@ -224,7 +224,7 @@ int main(int argc, char *argv[])
 //	hypertune(pnet, x_train, y_train, x_train->rows / 20);
 
 	// train the network
-	ann_train_network(pnet, x_train, y_train, x_train->rows);
+	ann_train_network(pnet, x_train, y_train, x_train->rows / 20);
 	
 	// evaluate the network against the test data
 	real acc = ann_evaluate(pnet, x_test, y_test);
